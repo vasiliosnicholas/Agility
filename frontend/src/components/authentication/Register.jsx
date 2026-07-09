ce889200c23159813870a1667efe1344778020da
vasiliosnicholas <vasiliosnicholasgr@gmail.com>
2026-07-09 17:24:33 -0400

    Add account type selector

--- frontend/src/components/authentication/Register.jsx
+++ frontend/src/components/authentication/Register.jsx
@@ -1,9 +1,31 @@
 import { useState } from "react";
 import { Form, FloatingLabel } from "react-bootstrap";
 
+const accountTypes = [
+  { name: "Manager", action: () => console.log("Manager Placeholder action") },
+  {
+    name: "Developer",
+    action: () => console.log("Developer Placeholder action"),
+  },
+];
+
 export default function Register() {
+  const [selectedAccountType, setAccountType] = useState(undefined);
   return (
     <Form>
+      <FloatingLabel
+        className="mb-3"
+        controlId="account-type"
+        label="Account Type"
+      >
+        <Form.Select aria-label="Select Account Type" defaultValue={undefined}>
+          <option isInvalid>Select Account Type</option>
+          {accountTypes.map((type, index) => (
+            <option key={type.name} value={type.name}>{type.name}</option>
+          ))}
+        </Form.Select>
+      </FloatingLabel>
+
       <FloatingLabel className="mb-3" controlId="email" label="Email Address">
         <Form.Control type="email" placeholder="name@example.com" autoFocus />
       </FloatingLabel>
