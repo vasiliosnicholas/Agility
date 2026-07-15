826a5588bd76fb66ea3387df555308db2c75689c
vasiliosnicholas <vasiliosnicholasgr@gmail.com>
2026-07-15 01:08:51 -0400

    fix typo

--- src/backend/database/Database.ts
+++ src/backend/database/Database.ts
@@ -19,7 +19,7 @@ let connectionTimeout: ReturnType<typeof setTimeout> | undefined | null;
 
 if (!URI || !DB_NAME || !USERS_COLLECTION || !TICKETS_COLLECTION) {
   throw new Error(
-    `MONGODB_URI, DB_NAME, DB_USERS_COLLECTION_NAME, and DB_TICKETS_COLLECTION_NAME are all required env variables`;
+    `MONGODB_URI, DB_NAME, DB_USERS_COLLECTION_NAME, and DB_TICKETS_COLLECTION_NAME are all required env variables`
   );
 }
 
@@ -62,31 +62,39 @@ async function getAgilityDB() {
 }
 
 export interface UserDocument extends Omit<User, "_id"> {
-  _id : ObjectId | string;
+  _id: ObjectId | string;
 }
 
 export function convertToUserDocument(user: User): UserDocument {
-  return {...user, _id: new ObjectId(user._id)}; //should work since unpack everything first, then overwrite _id with ObjectId
+  return { ...user, _id: new ObjectId(user._id) }; //should work since unpack everything first, then overwrite _id with ObjectId
 }
 
 export function convertToUser(userDocument: UserDocument): User {
-  const id = (typeof userDocument._id == "string") ? userDocument._id : userDocument._id.toHexString()
-  const user = {...userDocument, _id : id};
+  const id =
+    typeof userDocument._id == "string"
+      ? userDocument._id
+      : userDocument._id.toHexString();
+  const user = { ...userDocument, _id: id };
   return user;
 }
 
 export async function getUsersCollection() {
   const db = await getAgilityDB();
-  if (USERS_COLLECTION && db) return db.collection<UserDocument>(USERS_COLLECTION);
-  throw new Error("Could not connect to Agility Db or could not find users collection in db");
+  if (USERS_COLLECTION && db)
+    return db.collection<UserDocument>(USERS_COLLECTION);
+  throw new Error(
+    "Could not connect to Agility Db or could not find users collection in db"
+  );
 }
 
 export async function getTicketsCollection() {
   const db = await getAgilityDB();
   //FIXME: Add the TicketDocument interface as a generic to collection call
-  if (TICKETS_COLLECTION && db) return db.collection(TICKETS_COLLECTION); 
-  throw new Error("Could not connect to Agility Db or could not find tickets collection in db");
+  if (TICKETS_COLLECTION && db) return db.collection(TICKETS_COLLECTION);
+  throw new Error(
+    "Could not connect to Agility Db or could not find tickets collection in db"
+  );
 }
 
 //FIXME: add this somewhere else
-(await getUsersCollection()).createIndex({username: 1}, {unique: true})
+(await getUsersCollection()).createIndex({ username: 1 }, { unique: true });
