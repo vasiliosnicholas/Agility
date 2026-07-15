2edcc70b9df85065250d8f9f11d764f6bbb3b141
vasiliosnicholas <vasiliosnicholasgr@gmail.com>
2026-07-15 14:52:27 -0400

    comment namespace before I forget why I need it

--- src/backend/authentication/express.d.ts
+++ src/backend/authentication/express.d.ts
@@ -1,5 +1,8 @@
 import type { User as AgilityUser } from "../../shared/models/Users.ts";
 
+/**
+ * Needed to define Express.User as our User interface.
+ */
 declare global {
   namespace Express {
     interface User extends AgilityUser {}
