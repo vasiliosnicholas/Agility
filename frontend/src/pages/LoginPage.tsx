673fbab0cbf1f1a4130ec22762016f27ffad0a08
vasiliosnicholas <vasiliosnicholasgr@gmail.com>
2026-07-16 14:12:37 -0400

    Basic login page with conditional message

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,9 +2,23 @@ import { Container } from "react-bootstrap";
 import AuthWindow from "../components/authentication/AuthWindow.tsx";
 export default function Login() {
   return (
-    <Container className="d-flex flex-column min-vh-100 align-items-center">
-      <h1 className="text-center"> You have been signed out</h1>
-      <AuthWindow></AuthWindow>
+    <Container className="d-flex flex-column align-items-center justify-content-center min-vh-100">
+      <main className="d-flex flex-column align-items-center justify-content-center bg-warning rounded-2 p-3">
+        <header className="d-flex flex-column align-items-center bg-body rounded-2 p-2">
+          <h1 className="text-center">
+            {" "}
+            {`${
+              window.location.hash ? "You have been signed out" : "Unauthorized"
+            }`}
+          </h1>
+          <h2>
+            Please login{!window.location.hash ? " or register" : ""} to
+            continue
+          </h2>
+        </header>
+
+        <AuthWindow></AuthWindow>
+      </main>
     </Container>
   );
 }
